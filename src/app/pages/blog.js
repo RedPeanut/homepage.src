c1f34a5fa46b12109c0f3ccd9a337c86102c1331
김진규 <zamong99@gmail.com>
2023-05-20 10:31:05 +0900

    잔수정

--- src/app/pages/blog.js
+++ src/app/pages/blog.js
@@ -13,7 +13,7 @@ class Blog extends React.Component {
 
   render() {
 
-    console.log('this.props = ', this.props);
+    if(process.env.NODE_ENV === 'development') console.log('this.props = ', this.props);
     const pages = this.props.pages ? this.props.pages : this.props.route.pages;
     const path = this.props.route.path === '' || this.props.route.path === '/' ? 'blog' : this.props.route.path;
     const sortedPages = _.sortBy(pages, 'data.insert').reverse();
