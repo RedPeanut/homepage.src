0f37b435d4fd6f1184192815389dcc17d4bc33c4
김진규 <zamong99@gmail.com>
2023-05-25 20:55:00 +0900

    [코드정리] 들여쓰기

--- src/app/tools/color/converter.js
+++ src/app/tools/color/converter.js
@@ -115,54 +115,68 @@ class ColorConverter extends React.Component {
 
   return (
     <div className="w3-container w3-padding-large ws-grey" style={{borderRadius:"5px"}}>
-    <div className="w3-row">
-      <div className="w3-col l6">
-        <h2>Enter a Color:</h2>
-        <label><span className="xw3-text-grey"><i>name, hex, rgb, hsl, hwb, cmyk, ncol:</i></span></label>
-        <input ref="color01" id="color01" type="text" value={this.state.color01 || ""}  className="w3-input w3-border"
-          onChange={this.onChangeColor01} onKeyDown={this.onKeyDownColor01}/>
-        <br/>
-        <div className="resultStrings">
-          <div id="error01"></div>
-          <table id="resultTable" style={{display:"table"}}>
-            <tbody>
-              <tr>
-                <td id="helpname01">Name</td><td id="name01" style={{fontStyle:"normal", color:"rgb(0, 0, 0)"}}>DeepSkyBlue</td>
-              </tr>
-              <tr>
-                <td id="helprgb01"><span data-href="colors_rgb.asp?color=rgb(0, 191, 255)">Rgb</span></td><td id="rgb01">rgb(0, 191, 255)</td>
-              </tr>
-              <tr>
-                <td id="helphex01"><span data-href="colors_hexadecimal.asp?color=00bfff">Hex</span></td><td id="hex01">#00bfff</td>
-              </tr>
-              <tr>
-                <td id="helphsl01"><span data-href="colors_hsl.asp?color=hsl(195, 1, 0.5)">Hsl</span></td><td id="hsl01">hsl(195, 100%, 50%)</td>
-              </tr>
-              <tr>
-                <td id="helphwb01"><span data-href="colors_hwb.asp?color=hwb(195, 0, 0)">Hwb</span></td><td id="hwb01">hwb(195, 0%, 0%)</td>
-              </tr>
-              <tr>
-                <td id="helpcmyk01"><span data-href="colors_cmyk.asp?color=cmyk(1, 0.25, 0, 0)">Cmyk</span></td><td id="cmyk01">cmyk(100%, 25%, 0%, 0%)</td>
-              </tr>
-              <tr>
-                <td id="helpncol01"><span data-href="colors_ncol.asp?color=C25, 0, 0">Ncol</span></td><td id="ncol01">C25, 0%, 0%</td>
-              </tr>
-              {/* <!--<tr><td id="helpasterix" colspan="2">*Not a web standard.</td></tr>--> */}
-            </tbody>
-          </table>
+      <div className="w3-row">
+        <div className="w3-col l6">
+          <h2>Enter a Color:</h2>
+          <label><span className="xw3-text-grey"><i>name, hex, rgb, hsl, hwb, cmyk, ncol:</i></span></label>
+          <input ref="color01" id="color01" type="text" value={this.state.color01 || ""}  className="w3-input w3-border"
+            onChange={this.onChangeColor01} onKeyDown={this.onKeyDownColor01}/>
+          <br/>
+          <div className="resultStrings">
+            <div id="error01"></div>
+            <table id="resultTable" style={{display:"table"}}>
+              <tbody>
+                <tr>
+                  <td id="helpname01">Name</td>
+                  <td id="name01" style={{fontStyle:"normal", color:"rgb(0, 0, 0)"}}>DeepSkyBlue</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helprgb01"><span data-href="colors_rgb.asp?color=rgb(0, 191, 255)">Rgb</span></td>
+                  <td id="rgb01">rgb(0, 191, 255)</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helphex01"><span data-href="colors_hexadecimal.asp?color=00bfff">Hex</span></td>
+                  <td id="hex01">#00bfff</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helphsl01"><span data-href="colors_hsl.asp?color=hsl(195, 1, 0.5)">Hsl</span></td>
+                  <td id="hsl01">hsl(195, 100%, 50%)</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helphwb01"><span data-href="colors_hwb.asp?color=hwb(195, 0, 0)">Hwb</span></td>
+                  <td id="hwb01">hwb(195, 0%, 0%)</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helpcmyk01"><span data-href="colors_cmyk.asp?color=cmyk(1, 0.25, 0, 0)">Cmyk</span></td>
+                  <td id="cmyk01">cmyk(100%, 25%, 0%, 0%)</td>
+                  <td></td>
+                </tr>
+                <tr>
+                  <td id="helpncol01"><span data-href="colors_ncol.asp?color=C25, 0, 0">Ncol</span></td>
+                  <td id="ncol01">C25, 0%, 0%</td>
+                  <td></td>
+                </tr>
+                {/* <!--<tr><td id="helpasterix" colspan="2">*Not a web standard.</td></tr>--> */}
+              </tbody>
+            </table>
+          </div>
+        </div>
+        <div className="w3-col l6" style={{padding:"40px 40px 10px 40px"}}>
+          <div id="behindresult01">
+            <div style={{height:"294px", backgroundColor:"rgb(0, 191, 255)"}} id="result01">&nbsp;</div>
+          </div>
         </div>
       </div>
-      <div className="w3-col l6" style={{padding:"40px 40px 10px 40px"}}>
-        <div id="behindresult01">
-          <div style={{height:"294px", backgroundColor:"rgb(0, 191, 255)"}} id="result01">&nbsp;</div>
+      {/* <div className="w3-row">
+        <div className="w3-col">
+        <div id="linktocp"><hr style={{borderColor:"#dfdfdf"}}/><p><a href="colors_picker.asp?color=00bfff">Use this color in our Color Picker</a></p></div>
         </div>
-      </div>
-    </div>
-    {/* <div className="w3-row">
-      <div className="w3-col">
-      <div id="linktocp"><hr style={{borderColor:"#dfdfdf"}}/><p><a href="colors_picker.asp?color=00bfff">Use this color in our Color Picker</a></p></div>
-      </div>
-    </div> */}
+      </div> */}
     </div>
   )
   }
